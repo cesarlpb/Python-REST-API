8518b30d183548e909c18c8d7ae03add1feeb13f
César <cesarlpb@gmail.com>
2023-02-10 17:04:06 +0100

    Django REST Framework - setup

---
+++ Django-REST-Framework/countries/models.py
@@ -0,0 +1,7 @@
+# countries/models.py
+from django.db import models
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    capital = models.CharField(max_length=100)
+    area = models.IntegerField(help_text="(in square kilometers)")
