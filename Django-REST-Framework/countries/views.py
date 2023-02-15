ae3cfe7857b19eb1115d0574ee1d3aa6b14919ab
César <cesarlpb@gmail.com>
2023-02-15 16:37:47 +0100

    Terminamos el tutorial de REST Framework

--- Django-REST-Framework/countries/views.py
+++ Django-REST-Framework/countries/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Country
+from .serializers import CountrySerializer
+
+class CountryViewSet(viewsets.ModelViewSet):
+    serializer_class = CountrySerializer
+    queryset = Country.objects.all()
